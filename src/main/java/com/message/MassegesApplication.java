834573d4adf615649531ae14359b7ac97df34bdd
pavansrinivas.m <pavansrinivas.m@procreate.co.in>
2023-04-17 15:23:34 +0530

    first commit

---
+++ src/main/java/com/message/MassegesApplication.java
@@ -0,0 +1,13 @@
+package com.message;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MassegesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MassegesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/message/MassegesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.message;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MassegesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
